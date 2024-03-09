927a9047936619106854570c5eb28ffb7da2636a
a.basavets <leshabosovec@gmail.com>
2024-03-09 23:48:48 +0300

    change_to_session_factory

--- src/main/java/basavets/test/repository/impl/CustomSumRepositoryImpl.java
+++ src/main/java/basavets/test/repository/impl/CustomSumRepositoryImpl.java
@@ -2,46 +2,53 @@ package basavets.test.repository.impl;
 
 import basavets.test.entity.Data;
 import basavets.test.repository.CustomSumRepository;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
+import basavets.test.utils.HibernateUtils;
 import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaDelete;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Root;
-import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import org.hibernate.Session;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
+@AllArgsConstructor
 public class CustomSumRepositoryImpl implements CustomSumRepository {
 
     private static final String NAME = "name";
-    @PersistenceContext
-    private EntityManager entityManager;
+    private final HibernateUtils hibernateUtils;
 
     @Override
-    @Transactional
     public void save(Data data) {
-        entityManager.persist(data);
+        try (Session session = hibernateUtils.getSessionFactory().getCurrentSession()) {
+            session.beginTransaction();
+            session.persist(data);
+            session.getTransaction().commit();
+        }
     }
 
     @Override
-    @Transactional
     public void delete(Data data) {
-        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
-        CriteriaDelete<Data> criteriaDelete = criteriaBuilder.createCriteriaDelete(Data.class);
-        Root<Data> dataRoot = criteriaDelete.from(Data.class);
-        criteriaDelete.where(criteriaBuilder.equal(dataRoot.get(NAME), data.getName()));
-        entityManager.createQuery(criteriaDelete).executeUpdate();
+        try (Session session = hibernateUtils.getSessionFactory().getCurrentSession()) {
+            session.beginTransaction();
+            session.remove(data);
+            session.getTransaction().commit();
+        }
     }
 
     @Override
     public Optional<Data> findByName(String name) {
-        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
-        CriteriaQuery<Data> criteriaQuery = criteriaBuilder.createQuery(Data.class);
-        Root<Data> dataRoot = criteriaQuery.from(Data.class);
-        criteriaQuery.select(dataRoot).where(criteriaBuilder.equal(dataRoot.get(NAME), name));
-        return entityManager.createQuery(criteriaQuery).getResultList().stream().findAny();
+        Optional<Data> data;
+        try (Session session = hibernateUtils.getSessionFactory().getCurrentSession()) {
+            session.beginTransaction();
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<Data> criteriaQuery = criteriaBuilder.createQuery(Data.class);
+            Root<Data> dataRoot = criteriaQuery.from(Data.class);
+            criteriaQuery.select(dataRoot).where(criteriaBuilder.equal(dataRoot.get(NAME), name));
+            data = session.createQuery(criteriaQuery).getResultList().stream().findAny();
+            session.getTransaction().commit();
+        }
+        return data;
     }
 }
